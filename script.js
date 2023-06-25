45b5ad749706fd63fb8ce9c4f2d8ddc059a63328
Om-Vilas-Kulkarni <kulkarniom-cmpn@atharvacoe.ac.in>
2023-06-25 13:24:09 +0530

    Drizzle section added
    Drizzle section added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ start();
 async function start() {
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=8172fb42178e324f967eab8091b520bb&units=metric`);
     let data = await response.json();
+    console.log(data);
     document.getElementById('citydes').innerHTML = data.name + " City," + " " + data.sys.country;
     document.getElementById('temperature').innerHTML = Math.floor(data.main.temp);
     document.getElementById('h11').innerHTML = data.weather[0].main;
@@ -36,13 +37,19 @@ async function start() {
         document.getElementById('div1').innerHTML=`<img src="images/snow.png" alt="error loading image">`;
         document.body.style.backgroundImage=`linear-gradient(160deg,rgba(12,12,12,0.2),rgba(12,12,12,0.2)),url('images/snowing.gif')`;
         document.getElementById('currentclimate').innerHTML = "Snowfall";
-        document.getElementById('h12').innerHTML = "high";
+        document.getElementById('h12').innerHTML = "High";
     }
     else if(w=="Mist"){
         document.getElementById('div1').innerHTML=`<img src="images/mist.png" alt="error loading image">`;
         document.body.style.backgroundImage=`linear-gradient(160deg,rgba(12,12,12,0.2),rgba(12,12,12,0.2)),url('images/misting.gif')`;
         document.getElementById('currentclimate').innerHTML = "Mist";
         document.getElementById('h12').innerHTML = "Normal";
+    }
+    else if(w=="Drizzle"){
+        document.getElementById('div1').innerHTML=`<img src="images/drizzle.png" alt="error loading image">`;
+        document.body.style.backgroundImage=`linear-gradient(160deg,rgba(12,12,12,0.2),rgba(12,12,12,0.2)),url('images/drizzling.gif')`;
+        document.getElementById('currentclimate').innerHTML = "Light Showers";
+        document.getElementById('h12').innerHTML = "High";
     }
     
     let precision=Math.round(data.main.temp);
@@ -114,6 +121,12 @@ async function CheckWeather() {
         document.body.style.backgroundImage=`linear-gradient(160deg,rgba(12,12,12,0.2),rgba(12,12,12,0.2)),url('images/misting.gif')`;
         document.getElementById('currentclimate').innerHTML = "Mist";
         document.getElementById('h12').innerHTML = "Normal";
+    }
+    else if(w=="Drizzle"){
+        document.getElementById('div1').innerHTML=`<img src="images/drizzle.png" alt="error loading image">`;
+        document.body.style.backgroundImage=`linear-gradient(160deg,rgba(12,12,12,0.2),rgba(12,12,12,0.2)),url('images/drizzling.gif')`;
+        document.getElementById('currentclimate').innerHTML = "Light Showers";
+        document.getElementById('h12').innerHTML = "High";
     }
     
     document.getElementById('h11').innerHTML = data.weather[0].main;
